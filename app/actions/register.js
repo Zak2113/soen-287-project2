3b8f347a7be1939060632dd6b1b03fbe05f99905
zak <zak.abdi211@gmail.com>
2026-04-04 00:43:27 -0400

    Added ZOD validation

--- app/actions/register.js
+++ app/actions/register.js
@@ -6,37 +6,44 @@ import { users } from "@/db/schema";
 import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { SignupSchema } from "@/app/lib/definitions";
 
 export async function registerUser(formData) {
-  const firstName = formData.get("firstName");
-  const lastName = formData.get("lastName");
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const role = formData.get("role") || "student"; // Default to student
 
-  // 1. Check if user exists
-  const existing = await db.select().from(users).where(eq(users.email, email));
-  if (existing.length > 0) {
-    return { error: "User already exists" };
-  }
+    const rawFormData = Object.fromEntries(formData.entries());
+    const validatedFields = SignupSchema.safeParse(rawFormData);
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing Fields. Failed to Register User.",
+        };
+    }
+    const { firstName, lastName, email, password, role } = validatedFields.data;
+    
 
-  // 2. Hash password
-  const hashedPassword = await bcrypt.hash(password, 10);
+    // 1. Check if user exists
+    const existing = await db.select().from(users).where(eq(users.email, email));
+    if (existing.length > 0) {
+        return { error: "User already exists" };
+    }
 
-  // 3. Insert into Drizzle
-  try {
-    await db.insert(users).values({
-      id: crypto.randomUUID(),
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      role: role,
-    });
-  } catch (err) {
-    return { error: "Database insertion failed" };
-  }
+    // 2. Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  // 4. Send them to login
-  redirect("/login");
+    // 3. Insert into Drizzle
+    try {
+        await db.insert(users).values({
+            id: crypto.randomUUID(),
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            role: role,
+        });
+    } catch (err) {
+        return { error: "Database insertion failed" };
+    }
+
+    // 4. Send them to login
+    redirect("/login");
 }
---
+++ app/lib/definitions.js
@@ -0,0 +1,27 @@
+// app/lib/definitions.js
+import { z } from 'zod';
+
+export const SignupSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, { message: 'First name must be at least 2 characters long.' })
+    .trim(),
+  lastName: z
+    .string()
+    .min(2, { message: 'Last name must be at least 2 characters long.' })
+    .trim(),
+  email: z
+    .string()
+    .email({ message: 'Please enter a valid email.' })
+    .trim(),
+  password: z
+    .string()
+    .min(8, { message: 'Be at least 8 characters long' })
+    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+    .regex(/[0-9]/, { message: 'Contain at least one number.' })
+    .trim(),
+
+    role: z.enum(["student", "admin"], {
+    errorMap: () => ({ message: "Invalid role selected." }),
+  }),
+});
