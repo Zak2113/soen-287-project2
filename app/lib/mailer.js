e3fc0da637f6f6ff1ab77934106e94a1f996afe9
Zak Abdi <zak.abdi211@gmail.com>
2026-04-05 19:52:24 -0400

    Updated nodemail logic

--- app/lib/mailer.js
+++ app/lib/mailer.js
@@ -2,10 +2,13 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "mail.zakabdi.dev",
-  port: 465,
-  secure: true, // true for port 465
+  port: 587,
+  secure: false, // true for port 465
   auth: {
     user: "noodle-noreply@zakabdi.dev",
     pass: process.env.SMTP_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
